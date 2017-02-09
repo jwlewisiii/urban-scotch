147da08a430f7cc8f83ba4dfee7e0e7b0a94fe8c
Dev <jameswlewisiii@gmail.com>
2017-02-09 17:21:20 -0500

    Added phpunit to composer file and wrote some more of the sync table command.

--- src/Database/Commands/SyncTableCommand.php
+++ src/Database/Commands/SyncTableCommand.php
@@ -13,6 +13,10 @@ use Ssh\Connection;
 
 class SyncTableCommand extends Command
 {
+    private $connection;
+    private $database;
+    private $table;
+
     protected function configure()
     {
       $this->setName('database:sync-table')
@@ -20,37 +24,85 @@ class SyncTableCommand extends Command
             remote database table by the same name.')
         ->setHelp('Help')
         ->addArgument('database', InputArgument::REQUIRED, 'The database name.')
-        ->addArgument('table-name', InputArgument::REQUIRED, 'The database table name.');
+        ->addArgument('table', InputArgument::REQUIRED, 'The database table name.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->database = $input->getArgument('database');
+        $this->table = $input->getArgument('table');
+
         $output->writeln('Synchronizing '
-            . $input->getArgument('database')
+            . $databaseName
             . "."
-            . $input->getArgument('table-name')
+            . $tableName
         );
 
-        $connection = $this->connectToServer();
+        $this->connectToServer();
+
+        // Close ssh connection.
+        unset($this->connection);
     }
 
     /**
      * Attempts to connect to remote server.
      * @return Ssh/Connection
      */
-    private function connectToServer()
+    protected function connectToServer()
     {
         $host = getenv('SERVER_HOST');
         $username = getenv('SERVER_USERNAME');
         $password = getenv('SERVER_PASSWORD');
 
         try {
-            $connection = new Connection($host);
-            $connection->authenticate($username, $password);
+            $this->connection = new Connection($host);
+            $this->connection->authenticate($username, $password);
         } catch(Exception $e) {
             echo 'Caught exception: ',  $e->getMessage(), "\n";
         }
+    }
 
-        return $connection;
+    /**
+     * @return array
+     */
+    protected getRemoteTable()
+    {
+        $file = $this->dumpTable();
+        $this->fetchTable($file);
+    }
+
+    /**
+     * @return string
+     */
+    protected function dumpTable()
+    {
+        $time = time();
+        $username = getenv('REMOTE_USERNAME');
+        $password = getenv('REMOTE_PASSWORD');
+        $filename = "{$this->database}_{$this->table}_$time.sql";
+        $cmd = "mysqldump -u $username -p$password
+            $this->database $this->table > $filename";
+
+        return isset($this->connection->execute($cmd)) ? $filename : false;
+    }
+
+    /**
+     * @param string file database table file
+     * @return string
+     */
+    protected function fetchTable($file)
+    {
+
+        /**
+         * @todo Duplicate code - Refactor
+         */
+        $host = getenv('SERVER_HOST');
+        $username = getenv('SERVER_USERNAME');
+        $password = getenv('SERVER_PASSWORD');
+        $ftpConnection = ftp_connect($host);
+
+        if(ftp_login($ftpConnection, $username, $password)) {
+            ftp_get($ftpConnection, $file);
+        }
     }
 }
