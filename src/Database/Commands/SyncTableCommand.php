678d6d8578ad2cd64bab11ef47e4ad562945de98
Dev <jameswlewisiii@gmail.com>
2017-02-09 11:47:15 -0500

    Started buildling the connection object

--- src/Database/Commands/SyncTableCommand.php
+++ src/Database/Commands/SyncTableCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Ssh\Connection;
 
 
 class SyncTableCommand extends Command
@@ -26,8 +27,30 @@ class SyncTableCommand extends Command
     {
         $output->writeln('Synchronizing '
             . $input->getArgument('database')
-            . "." 
+            . "."
             . $input->getArgument('table-name')
         );
+
+        $connection = $this->connectToServer();
+    }
+
+    /**
+     * Attempts to connect to remote server.
+     * @return Ssh/Connection
+     */
+    private function connectToServer()
+    {
+        $host = getenv('SERVER_HOST');
+        $username = getenv('SERVER_USERNAME');
+        $password = getenv('SERVER_PASSWORD');
+
+        try {
+            $connection = new Connection($host);
+            $connection->authenticate($username, $password);
+        } catch(Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+
+        return $connection;
     }
 }
---
+++ src/Ssh/Connection.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ssh;
+
+class Connection {
+
+  private $connection;
+  private $host;
+  private $username;
+  private $password;
+  private $post;
+  private $isAuthenticated;
+
+  function __construct($host, $port = 22)
+  {
+    $this->host = $host;
+    $this->port = $port;
+    $this->connection = ssh2_connect($this->host, $this->port);
+  }
+
+  public function authenticate($username, $password)
+  {
+    $this->username = $username;
+    $this->password = $password;
+
+    $this->isAuthenticated = ssh2_auth_password(
+      $this->connection,
+      $this->username,
+      $this->password
+    );
+
+    if(!$this->isAuthenticated)
+        throw new Exception("Unable to authenticate user, $username.");
+
+    return $this->isAuthenticated;
+  }
+
+  public function execute($command)
+  {
+    $stream = ssh2_exec($this->connection, $command);
+    stream_set_blocking($stream, true);
+    $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+    return stream_get_contents($stream_out);
+  }
+
+}
