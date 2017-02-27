bd0ca87753750b14fe23860827dbbd766cb21140
root <jameswlewisiii@gmail.com>
2017-02-27 12:05:01 -0500

    More work on table list command.

--- src/Database/Commands/ListTablesCommand.php
+++ src/Database/Commands/ListTablesCommand.php
@@ -1,19 +1,16 @@
 <?php
 namespace Database\Commands;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Database\Commands\UrbanScotchDbCommand;
 use Ssh\Connection;
 
 
-class ListTablesCommand extends Command
+class ListTablesCommand extends UrbanScotchDbCommand
 {
-    private $connection;
-    private $database;
 
     protected function configure()
     {
@@ -21,11 +18,32 @@ class ListTablesCommand extends Command
         ->setDescription('List all the tables for the desired remote database.')
         ->setHelp('Help')
         ->addArgument('database', InputArgument::REQUIRED, 'The database name.')
+        ->addOption('both', 'b', InputOption::VALUE_NONE, 'Display both remote and local tables.')
+        ->addOption('local', 'l', InputOption::VALUE_NONE, 'Display only local tables.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->database = $input->getArgument('database');
+        $output->writeln("TABLES:");
 
         //Ssh to server.
+        $this->connectToServer();
+        if($input->getOption('both')) {
+            $remoteTables = $this->getTableList();
+
+        } else {
+            $results = $this->getTableList();
+        }
+
+        $output->writeln($results);
     }
+
+    protected function getTableList()
+    {
+        $username = getenv('REMOTE_USERNAME');
+        $password = getenv('REMOTE_PASSWORD');
+        $cmd = "mysql -u $username -p$password -e 'SHOW TABLES' $this->database";
+        return $this->connection->execute($cmd);
+    }
+}
